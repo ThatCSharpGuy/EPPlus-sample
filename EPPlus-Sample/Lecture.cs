890617300d1889912457d17a1f3b7eb21e7088b4
Antonio Feregrino <antonio.feregrino@gmail.com>
2016-02-21 09:38:42 -0600

    Base

---
+++ EPPlus-Sample/Lecture.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPPlus_Sample
+{
+    public class Lecture
+    {
+        public int Id { get; set; }
+        public int TeacherId { get; set; }
+        public string Name { get; set; }
+        public string Level { get; set; }
+    }
+}
---
+++ EPPlus-Sample/Teacher.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPPlus_Sample
+{
+    public class Teacher
+    {
+        public int Id { get; set; }
+        public string GivenName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
+    }
+}
